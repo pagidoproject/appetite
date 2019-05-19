51e9df9b5ae5dbf3840d1e56e0942c81fa7fbc6a
pagidoproject <48087027+pagidoproject@users.noreply.github.com>
2019-05-19 12:47:22 +0530

    Add files via upload

---
+++ appetite (1).sql
@@ -0,0 +1,43 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 19, 2019 at 09:15 AM
+-- Server version: 10.1.37-MariaDB
+-- PHP Version: 7.3.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `appetite`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `fform`
+--
+
+CREATE TABLE `fform` (
+  `Id` varchar(10) NOT NULL,
+  `Name` varchar(20) NOT NULL,
+  `Email` varchar(30) NOT NULL,
+  `Area` varchar(30) NOT NULL,
+  `City` varchar(20) NOT NULL,
+  `State` varchar(25) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
